ba7a429a1370a1039721ecfd7c0fe9ddbc62c292
MEMZ-Chrome <96772167+MEMZ-Chrome@users.noreply.github.com>
2025-01-22 20:34:40 +0800

    Update FooterComponent.vue

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,20 +1,5 @@
 <template>
   <footer>
-    <span>富强</span>
-    <span>民主</span>
-    <span>文明</span>
-    <span>和谐</span>
-    <span>自由</span>
-    <span>平等</span>
-    <span>公正</span>
-    <span>法治</span>
-    <span>爱国</span>
-    <span>敬业</span>
-    <span>诚信</span>
-    <span>友善</span>
-    <div class="icp">
-      <span>© 2024</span><a href="https://bsgun.cn" target="_blank">梦爱吃鱼</a>
-      <a v-if="icpNumber" href="https://beian.miit.gov.cn/" target="_blank">{{ icpNumber }}</a>
       <button class="logo-btn" @click="toggleTips">小提示</button>
     </div>
   </footer>
